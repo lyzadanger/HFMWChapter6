1de38c47608c215be71d7134616b83454f5596df
Bryan Fox <bryan@bryfox.com>
2011-10-04 13:41:35 +0100

    Works without JS.

--- shared/tartan-maker/generate.php
+++ shared/tartan-maker/generate.php
@@ -6,13 +6,22 @@ $name = $_POST['name'];
 $stripe = 2;
 $sett = array();
 for($i=0; $i < sizeof($_POST['colors']); $i++) {
-  $sett[] = array('color' => $_POST['colors'][$i],
-                  'count' => $_POST['sizes'][$i]);
+  // check for submitted blank values
+  if ($_POST['colors'][$i] && $_POST['sizes'][$i]) {
+    $sett[] = array('color' => $_POST['colors'][$i],
+                    'count' => $_POST['sizes'][$i]);
+  }
 }
 $tartan = new LyzaTartan($name, $sett, 1);
 $tartan->set_dynamic_scale(300);
 $tartan->set_stripe_size($stripe);
 
 $xml = $tartan->to_xml();
-print json_encode(array('name' => $tartan->get_nicename()));
+
+if ($_POST['redirect_to_image'] == "true") {
+  header('Location:  /image.php?name=' . $_POST['name'] . '&width=320');
+  exit();
+} else {
+  print json_encode(array('name' => $tartan->get_nicename()));
+}
 ?>
--- shared/tartan-maker/index.php
+++ shared/tartan-maker/index.php
@@ -7,59 +7,61 @@
 
   <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0b3/jquery.mobile-1.0b3.min.css" />
   <style type="text/css">
-    .color { 
+/*    .color { 
       padding-left: 30px;
       background-image: -webkit-linear-gradient(90deg,rgba(0,0,0,.3),  transparent);
     }
+*/
   </style>
 
   <script type="text/javascript" src="http://code.jquery.com/jquery-1.6.3.min.js"></script>
   <script type="text/javascript" src="http://code.jquery.com/mobile/1.0b3/jquery.mobile-1.0b3.min.js"></script>
   <script type="text/javascript">
-    $('[data-role="page"]').live('pagecreate', function() {
-
-      $('label[for^="color1"]').each(function() {
-        $(this).addClass('color');
-        var colorValue = $('#' + $(this).attr('for')).val();
-        $(this).css('backgroundColor', colorValue);
-      });
-      $('label[for^="color1"]').click(function() {
-        $('#size1').focus().select();
-      });
-      $('#addcolor').bind('click', function() {
-        var colorSelected = $('[name="color1"]').filter('[checked="true"]');
-        var colorValue    = $(colorSelected).val();
-        var colorID       = $(colorSelected).attr('id');
-        var colorName     = $('label[for="' + colorID + '"]').text();
-        var colorSize     = $('#size1').val();
-        var colorLI = $('<li>' + colorName + ' (' + colorSize + ')</li>');
-        $(colorLI).data('colorData', { color: colorValue, size: colorSize });
-        $(colorLI).click(function() { 
-          $(this).remove(); 
-          $('ul').listview('refresh'); 
-        });
-        $('#colorlist').append(colorLI);
-        $('ul').listview('refresh');
-      });
-      $("#buildtartan").bind('click', function() {
-        var tartanName = $('#tartan_name').val();
-        var colors = new Array();
-        var sizes = new Array();
-        $('#colorlist li').each(function() {
-          var colorData = $(this).data('colorData');
-          colors[colors.length] = colorData.color;
-          sizes[sizes.length] = colorData.size;
-        });
-        var formData = { colors: colors, sizes: sizes, name: tartanName };
-        $.post('generate.php', formData, function(data) {
-          var tartanImage = $('<img />');
-   
-          tartanImage.attr('src', 'image.php?name=' + data.name + '&width=320');
-          $('#tartanholder').append(tartanImage);
-        }, 'json');
-      });
-    });
+    // $('[data-role="page"]').live('pagecreate', function() {
+    // 
+    //      $('label[for^="color1"]').each(function() {
+    //        $(this).addClass('color');
+    //        var colorValue = $('#' + $(this).attr('for')).val();
+    //        $(this).css('backgroundColor', colorValue);
+    //      });
+    //      $('label[for^="color1"]').click(function() {
+    //        $('#size1').focus().select();
+    //      });
+    //      $('#addcolor').bind('click', function() {
+    //        var colorSelected = $('[name="color1"]').filter('[checked="true"]');
+    //        var colorValue    = $(colorSelected).val();
+    //        var colorID       = $(colorSelected).attr('id');
+    //        var colorName     = $('label[for="' + colorID + '"]').text();
+    //        var colorSize     = $('#size1').val();
+    //        var colorLI = $('<li>' + colorName + ' (' + colorSize + ')</li>');
+    //        $(colorLI).data('colorData', { color: colorValue, size: colorSize });
+    //        $(colorLI).click(function() { 
+    //          $(this).remove(); 
+    //          $('ul').listview('refresh'); 
+    //        });
+    //        $('#colorlist').append(colorLI);
+    //        $('ul').listview('refresh');
+    //      });
+    //      $("#buildtartan").bind('click', function() {
+    //        var tartanName = $('#tartan_name').val();
+    //        var colors = new Array();
+    //        var sizes = new Array();
+    //        $('#colorlist li').each(function() {
+    //          var colorData = $(this).data('colorData');
+    //          colors[colors.length] = colorData.color;
+    //          sizes[sizes.length] = colorData.size;
+    //        });
+    //        var formData = { colors: colors, sizes: sizes, name: tartanName };
+    //        $.post('generate.php', formData, function(data) {
+    //          var tartanImage = $('<img />');
+    //   
+    //          tartanImage.attr('src', 'image.php?name=' + data.name + '&width=320');
+    //          $('#tartanholder').append(tartanImage);
+    //        }, 'json');
+    //      });
+    //    });
   </script>
+
 </head> 
 <body> 
 
@@ -71,73 +73,61 @@
 
   <div data-role="content">  
     <form method="post" action="generate.php" id="tartanator_form" data-ajax="false">
+
       <div data-role="fieldcontain">
-        <label for="tartan_name" id="tartan_name_label">Name the tartan:</label>
-        <input type="text" name="tartan_name" id="tartan_name" />
+        <label for="name" id="tartan_name_label">Name the tartan:</label>
+        <input type="text" name="name" id="name" />
       </div>
+
+      <?php for ($i = 0; $i < 6; $i++): ?>
+
+        <div class="size" data-role="fieldcontain">
+          <label for="size1" id="size1-label">Size</label>
+          <input type="text" name="sizes[]" min="2" max="72" value="" id="size1" /> <!-- range -->
+        </div>
+
+        <div class="color" data-role="fieldcontain">
+          <label for="color1" class="select">Color:</label>
+          <select name="colors[]" id="color1">
+            <option value=""></option>
+            <option value="#000000">Black</option>
+            <option value="#ff3344">Rose</option>
+            <option value="#00cc00">Lt. Green</option>
+            <option value="#B0E0E6">Lt. Blue</option>
+            <option value="#cc9966">Lt. Tan</option>
+            <option value="#ffec00">Yellow</option>
+            <option value="#ffffff">White</option>
+            <option value="#cc0000">Red</option>
+            <option value="#008000">Green</option>
+            <option value="#274086">Blue</option>
+            <option value="#996600">Tan</option>
+            <option value="#65295f">Purple</option>
+            <option value="#cccccc">Grey</option>
+            <option value="#660000">Dk. Red</option>
+            <option value="#126846">Bluegreen</option>
+            <option value="#001144">Dk. Blue</option>
+            <option value="#663300">Brown</option>
+            <option value="#FF8C00">Orange</option>
+            <option value="#5b6333">Olive</option>
+            <option value="#29292b">V.Dk. Blue</option>
+            <option value="#3c516c">Dk. Slate</option>
+          </select>
+        </div>
+
+      <?php endfor; ?>
+
       <div data-role="fieldcontain">
-        <label for="size1" id="size1-label">Size</label>
-        <input type="range" name="size1" min="2" max="72" value="2" id="size1" />
+        <input type="hidden" name="redirect_to_image" value="true">
+        <input type="submit" data-role="button" name="addcolor" id="addcolor" value="Add This Color" data-icon="plus" />
       </div>
+
+      <ul data-role="listview" data-inset="true" id="colorlist">
+      </ul>
+
       <div data-role="fieldcontain">
+        <input type="submit" name="buildtartan" id="buildtartan" value="Make it!" data-role="button" />
+      </div>
 
-        <input type="radio" name="color1" id="color1-option-1" value="#000000" />
-        <label for="color1-option-1" id="color1-option-1-label" >Black</label>
-        <input type="radio" name="color1" id="color1-option-2" value="#ff3344" />
-        <label for="color1-option-2" id="color1-option-2-label" >Rose</label>
-        <input type="radio" name="color1" id="color1-option-3" value="#00cc00"  />
-        <label for="color1-option-3" id="color1-option-3-label" >Lt. Green</label>
-        <input type="radio" name="color1" id="color1-option-4" value="#B0E0E6"  />
-        <label for="color1-option-4" id="color1-option-4-label" >Lt. Blue</label>
-        <input type="radio" name="color1" id="color1-option-5" value="#cc9966"  />
-        <label for="color1-option-5" id="color1-option-5-label" >Lt. Tan</label>
-        <input type="radio" name="color1" id="color1-option-6" value="#ffec00"  />
-        <label for="color1-option-6" id="color1-option-6-label" >Yellow</label>
-
-        <input type="radio" name="color1" id="color1-option-7" value="#ffffff" />
-        <label for="color1-option-7" id="color1-option-7-label" >White</label>
-        <input type="radio" name="color1" id="color1-option-8" value="#cc0000" />
-        <label for="color1-option-8" id="color1-option-8-label" >Red</label>
-        <input type="radio" name="color1" id="color1-option-9" value="#008000"  />
-        <label for="color1-option-9" id="color1-option-9-label" >Green</label>
-        <input type="radio" name="color1" id="color1-option-10" value="#274086"  />
-        <label for="color1-option-10" id="color1-option-10-label" >Blue</label>
-        <input type="radio" name="color1" id="color1-option-11" value="#996600"  />
-        <label for="color1-option-11" id="color1-option-11-label" >Tan</label>
-        <input type="radio" name="color1" id="color1-option-12" value="#65295f"  />
-        <label for="color1-option-12" id="color1-option-12-label" >Purple</label>
-
-        <input type="radio" name="color1" id="color1-option-13" value="#cccccc" />
-        <label for="color1-option-13" id="color1-option-13-label" >Grey</label>
-        <input type="radio" name="color1" id="color1-option-14" value="#660000" />
-        <label for="color1-option-14" id="color1-option-14-label" >Dk. Red</label>
-        <input type="radio" name="color1" id="color1-option-15" value="#126846"  />
-        <label for="color1-option-15" id="color1-option-15-label" >Bluegreen</label>
-        <input type="radio" name="color1" id="color1-option-16" value="#001144"  />
-        <label for="color1-option-16" id="color1-option-16-label" >Dk. Blue</label>
-        <input type="radio" name="color1" id="color1-option-17" value="#663300"  />
-        <label for="color1-option-17" id="color1-option-17-label" >Brown</label>
-        <input type="radio" name="color1" id="color1-option-18" value="#FF8C00"  />
-        <label for="color1-option-18" id="color1-option-18-label" >Orange</label>
-
-        <input type="radio" name="color1" id="color1-option-19" value="#5b6333" />
-        <label for="color1-option-19" id="color1-option-19-label" >Olive</label>
-        <input type="radio" name="color1" id="color1-option-20" value="#29292b" />
-        <label for="color1-option-20" id="color1-option-20-label" >V.Dk. Blue</label>
-        <input type="radio" name="color1" id="color1-option-21" value="#3c516c" />
-        <label for="color1-option-21" id="color1-option-21-label" >Dk. Slate</label>
-
-    </div>
-
-    <div data-role="fieldcontain">
-      <input type="button" data-role="button" name="addcolor" id="addcolor" value="Add This Color" data-icon="plus" />
-    </div>
-    <ul data-role="listview" data-inset="true" id="colorlist">
-
-    </ul>
-    <div data-role="fieldcontain">
-      <input type="button" name="buildtartan" id="buildtartan" value="Make it!" data-role="button" />
-    </div>
     </form>
     
     <div id="tartanholder">
