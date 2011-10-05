f7680a1712e3cf1e38d84d843ed80fee07dc1d05
Bryan Fox <bryan@bryfox.com>
2011-10-05 09:33:24 +0100

    Use shared width (300)

--- shared/tartan-maker/generate.php
+++ shared/tartan-maker/generate.php
@@ -3,6 +3,7 @@ require('config.php');
 require('tartan.inc');
 
 $name = $_POST['name'];
+$width = 300;
 $stripe = 2;
 $sett = array();
 for($i=0; $i < sizeof($_POST['colors']); $i++) {
@@ -13,7 +14,7 @@ for($i=0; $i < sizeof($_POST['colors']); $i++) {
   }
 }
 $tartan = new LyzaTartan($name, $sett, 1);
-$tartan->set_dynamic_scale(300);
+$tartan->set_dynamic_scale($width);
 $tartan->set_stripe_size($stripe);
 
 $xml = $tartan->to_xml();
@@ -24,6 +25,6 @@ if (empty($name)) {
 }
 
 $redirect_path = ($_POST['redirect_to_image'] == "true") ? 'image.php' : 'post_create.php';
-header('Location:  /' . $redirect_path . '?name=' . $name . '&width=320');
+header('Location:  /' . $redirect_path . '?name=' . $name . '&width=' . $width);
 exit();
 
